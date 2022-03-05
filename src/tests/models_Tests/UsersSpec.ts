276e5528e1ea14332fc3624ea216ff65e85083d0
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-03-05 11:25:02 +0200

    adding some models tests

--- src/tests/models_Tests/UsersSpec.ts
+++ src/tests/models_Tests/UsersSpec.ts
@@ -1,42 +1,62 @@
-// import { User, UsersStore } from "../../models/User";
+import { User, UsersStore } from "../../models/User";
 
-// const store = new UsersStore();
+const store = new UsersStore();
 
-// describe("Create User Method", () => {
-//   it("should create a user", async () => {
-//     const user = {
-//       firstName: "John",
-//       lastName: "Doe",
-//       password: "12345",
-//     };
-//     const createdUser = await store.create(user);
-//     expect(createdUser).toBeDefined();
-//   });
-// });
+describe("Index Method Users", () => {
+  it("should return an array of users", async () => {
+    const users = await store.index();
+    expect(users).toBeInstanceOf(Array);
+  });
+});
 
-// describe("Login User Method", () => {
-//   it("should login a user", async () => {
-//     const user = {
-//       firstName: "John",
-//       password: "12345",
-//     };
-//     const loggedInUser = await store.login(user);
-//     expect(loggedInUser).toBeTruthy();
-//   });
-// });
+describe("Show User Method", () => {
+  it("should return a user", async () => {
+    const user = await store.show(1);
+    expect(user).toBeDefined();
+  });
+});
 
-// describe("Show User method", () => {
-//   it("should show a user", async () => {
-//     const id: number = 1;
-//     const showUser = await store.show(id);
-//     expect(showUser).toBeTruthy();
-//   });
-// });
+describe("Create User Method", () => {
+  it("should return a user", async () => {
+    // @ts-ignore
+    const newUser: User = {
+      firstName: "John",
+      lastName: "Doe",
+      password: "password",
+    };
+    const user = await store.create(newUser);
+    expect(user).toBeDefined();
+  });
+});
 
-// describe("Delete User Method", () => {
-//   it("should delete a user", async () => {
-//     const id: number = 1;
-//     const deletedUser = await store.deleteUser(id);
-//     expect(deletedUser).toBeTruthy();
-//   });
-// });
+describe("Login User Method", () => {
+  it("should return a user", async () => {
+    // @ts-ignore
+    const user: User = {
+      firstName: "John",
+      password: "password",
+    };
+    const user2 = await store.login(user);
+    expect(user2).toBeDefined();
+  });
+});
+
+describe("Update User Method", () => {
+  it("should return a user", async () => {
+    // @ts-ignore
+    const user: User = {
+      firstName: "David",
+      lastName: "Wagih",
+      password: "dazy123",
+    };
+    const user2 = await store.updateUser(1, user);
+    expect(user2).toBeDefined();
+  });
+});
+
+describe("Delete User Method", () => {
+  it("should return a user", async () => {
+    const user = await store.deleteUser(1);
+    expect(user).toBeDefined();
+  });
+});
