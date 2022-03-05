a87c146b4b6cbad1050728e64868ad9fcd0f660f
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-03-05 20:32:49 +0200

    changing the Sql commands and adding model and handler for the 4th table

--- migrations/sqls/20220227200132-tables-up.sql
+++ migrations/sqls/20220227200132-tables-up.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
       firstName VARCHAR(50),
       lastName VARCHAR(50),
       password text);
+
 INSERT INTO users (firstName, lastName, password) VALUES ('John', 'Doe', 'password');
 INSERT INTO users (firstName, lastName, password) VALUES ('John', 'Doe', 'password');
 INSERT INTO users (firstName, lastName, password) VALUES ('John', 'Doe', 'password');
@@ -14,18 +15,25 @@ CREATE TABLE products (
     name VARCHAR(64) NOT NULL,
     price integer NOT NULL
 );
+
 INSERT INTO products (name, price) VALUES ('Product 1', 100);
 INSERT INTO products (name, price) VALUES ('Product 2', 200);
 INSERT INTO products (name, price) VALUES ('Product 3', 300);
 INSERT INTO products (name, price) VALUES ('Product 4', 400);
 INSERT INTO products (name, price) VALUES ('Product 5', 500);
+
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    status VARCHAR(15),
-    user_id bigint REFERENCES users(id),
-    product_id bigint REFERENCES products(id),
-    quantity integer NOT NULL
+    status VARCHAR(15)
     );
+
+
+CREATE TABLE order_products( 
+    id SERIAL PRIMARY KEY, 
+    user_id bigint REFERENCES users(id), 
+    product_id bigint REFERENCES products(id), 
+    quantity integer NOT NULL
+);
 
 
    
